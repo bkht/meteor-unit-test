7550fc951ab1a890b39bc0fe934f890f95c8b660
JackBerkhout <info@jacksoft.eu>
2016-09-02 17:53:27 +0200

    Added createRandomNumbersArray

--- client/stringHelpers.tests.js
+++ client/stringHelpers.tests.js
@@ -1,6 +1,7 @@
 // meteor test --driver-package practicalmeteor:mocha --port 4000
 
 import {capitalizeFirstLetter, splitString, alternateString, partitionString} from './stringHelpers';
+import {createRandomNumbersArray} from './numberHelpers';
 
 describe('tests for stringHelpers', () => {
     it('should capitalize the first letter of an input string', () => {
@@ -26,5 +27,17 @@ describe('tests for stringHelpers', () => {
     });
     it('partitionString(\'helloworld\', 5) should convert into [[\'hello\'], [\'world\']]', () => {
         expect(partitionString('helloworld', 5)).to.eql([['hello'], ['world']]);
+    });
+    it('randomArray(min=50, max=100, n=3) should convert into DESC Array', () => {
+        // expect(createRandomNumbersArray(50, 100, 3)).to.eql([['hello'], ['world']]);
+        var myArray = createRandomNumbersArray(50, 100, 3);
+        // var myArray = [56, 68, 82];
+        var result1 = myArray[0];
+        var result2 = myArray[1];
+        var result3 = myArray[2];
+        expect(result1).to.be.within(50,100);
+        expect(myArray).to.have.lengthOf(3);
+        expect(result2).to.be.at.least(result1);
+        expect(result3).to.be.at.least(result2);
     });
 });
